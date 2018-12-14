578320749b8bea60727e14804bc9662645073e81
JoshWWC <joshuawcleveland@gmail.com>
2018-12-14 15:33:02 -0800

    Added behavior for submitting item, just need to fix styling now

--- src/js/todo-list.js
+++ src/js/todo-list.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Panel from './panel.js'
 
 class ToDoList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {itemArray: [], value: ''};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -16,18 +16,34 @@ class ToDoList extends React.Component {
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    this.createPanel();
     event.preventDefault();
+  }
+
+  createPanel() {
+    const item = this.state.itemArray;
+     item.push(
+       <div>
+         <p>{this.state.value}</p>
+       </div>
+     )
+     this.setState({itemArray: item})
   }
 
   render() {
     return (
+      <div>
       <form onSubmit={this.handleSubmit}>
         <label>
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="text" placeholder="New item..." value={this.state.value} onChange={this.handleChange} />
         </label>
         <input type="submit" value="" />
       </form>
+
+      {this.state.itemArray.map((item, index) => {
+         return <div key={index}>{item}</div>
+       })}
+       </div>
     );
   }
 }
